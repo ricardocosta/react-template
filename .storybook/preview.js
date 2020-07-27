b419eca07ae0cc75ba42251b5ca103671f877234
Ricardo Costa <ricardocosta1989@gmail.com>
2020-07-27 18:57:48 +0100

    chore: Setup Storybook
    Setup Storybook in the project, also including support for MDX files
    that will help in having storybook as documentation as well.
    
    Issue #1

---
+++ .storybook/preview.js
@@ -0,0 +1,6 @@
+import { withA11y } from "@storybook/addon-a11y";
+import { withKnobs } from "@storybook/addon-knobs";
+import { addDecorator } from "@storybook/react";
+
+addDecorator(withA11y);
+addDecorator(withKnobs);
